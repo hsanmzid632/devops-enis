b1ab4bcfde7a53512ff0c93ceb62deb233ad66ab
hsanmzid632 <hsan.mzid@gmail.com>
2024-12-11 00:34:39 +0100

    Add Jenkinsfile for CI/CD pipeline

--- enis-app-tp/frontend/src/components/Form.jsx
+++ enis-app-tp/frontend/src/components/Form.jsx
@@ -35,7 +35,7 @@ function Form({ route, method }) {
 
     return (
         <form onSubmit={handleSubmit} className="form-container">
-            <h1>{name}</h1>
+            <h1>{name}/Hsan Mzid</h1>
             <input
                 className="form-input"
                 type="text"
